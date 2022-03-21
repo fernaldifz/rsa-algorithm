d407bc2b36298ebdccb637153fbeeb9c7c93bd06
wpo9nine <wpo9nine@gmail.com>
2022-03-21 19:56:51 +0700

    init

---
+++ RSA.py
@@ -0,0 +1,21 @@
+def encrypt(): #enkripsi menggunakan RSA
+    pass
+
+def decrypt(): #dekripsi menggunakan RSA
+    pass
+
+def generatePublicKey(): #pembangkit kunci publik
+    pass
+
+def generatePrivateKey(): #pembangkit kunci privat
+    pass
+
+def toHex(): #khusus untuk ciphertext dalam notasi heksadesimal
+    pass
+
+def showTimeLapse(): #program dapat menampilkan interval waktu enkripsi / dekripsi berapa lama
+    pass
+
+def showFileSize(): #program dapat menampilkan size file hasil enkripsi / dekripsi
+    pass
+
