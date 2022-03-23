e4fe2bd2addcc3d88067bc200fd869be1e9b54b8
wpo9nine <wpo9nine@gmail.com>
2022-03-23 18:18:03 +0700

    add function

--- RSA.py
+++ RSA.py
@@ -1,13 +1,32 @@
+import random,math
+
+def generatePairNumbers():
+    primes = [i for i in range(2**8,(2**16)-1) if isPrime(i)]
+    p,q = 0,0
+
+    while p == q:
+        p,q = random.choice(primes),random.choice(primes)
+    
+    return p,q
+
+def isPrime(num):
+    if num == 2:
+        return True
+    elif num < 2:
+        return False
+    else:
+        for i in range(2,num,1):
+            if num % i == 0:
+                return False
+    return True
+
+def generatePairKey(): #pembangkit kunci publik
+    p,q = generatePairNumbers()
+
 def encrypt(): #enkripsi menggunakan RSA
     pass
 
 def decrypt(): #dekripsi menggunakan RSA
-    pass
-
-def generatePublicKey(): #pembangkit kunci publik
-    pass
-
-def generatePrivateKey(): #pembangkit kunci privat
     pass
 
 def toHex(): #khusus untuk ciphertext dalam notasi heksadesimal
@@ -19,3 +38,5 @@ def showTimeLapse(): #program dapat menampilkan interval waktu enkripsi / dekrip
 def showFileSize(): #program dapat menampilkan size file hasil enkripsi / dekripsi
     pass
 
+p,q = generatePairNumbers()
+print(p,q)
